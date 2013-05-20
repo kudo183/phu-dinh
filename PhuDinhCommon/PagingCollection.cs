af565722afdc358645f8a9f1b3a1ba050a07dcb0
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-05-20 03:36:55 +0000

    - init project with rPhuongTienView.

---
+++ PhuDinhCommon/PagingCollection.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhuDinhCommon
+{
+    public class PagingCollection<T>
+    {
+        private List<T> _list;
+
+        private int _pageCount;
+        public int PageCount
+        {
+            get { return _pageCount; }
+            private set { _pageCount = value; }
+        }
+
+        private int _currentPage;
+        public int CurrentPage
+        {
+            get { return this._currentPage; }
+            set
+            {
+                if (value <= 0)
+                {
+                    this._currentPage = 1;
+                }
+                else if (value > this._pageCount)
+                {
+                    this._currentPage = this._pageCount;
+                }
+                else
+                {
+                    this._currentPage = value;
+                }
+
+                this.UpdateCurrentPageItems();
+            }
+        }
+
+        private int _itemsPerPage;
+        public int ItemsPerPage
+        {
+            get { return _itemsPerPage; }
+            set
+            {
+                if (value <= 0)
+                {
+                    this._itemsPerPage = 1;
+                }
+                else if (value > this._list.Count)
+                {
+                    this._itemsPerPage = this._list.Count;
+                }
+                else
+                {
+                    this._itemsPerPage = value;
+                }
+
+                this.UpdatePageCount();
+                this.UpdateCurrentPageItems();
+            }
+        }
+
+        private List<T> _currentPageItems = new List<T>();
+        public List<T> CurrentPageItems
+        {
+            get { return _currentPageItems; }
+        }
+
+        private int EndIndex
+        {
+            get
+            {
+                if (this._itemsPerPage <= 0)
+                    return this._list.Count;
+
+                var end = this._currentPage * this._itemsPerPage;
+                return (end > this._list.Count) ? this._list.Count : end;
+            }
+        }
+
+        private int StartIndex
+        {
+            get
+            {
+                if (this._itemsPerPage <= 0)
+                    return 0;
+
+                return (this._currentPage - 1) * this._itemsPerPage;
+            }
+        }
+
+        public PagingCollection(List<T> list)
+        {
+            this._list = list;
+            this._currentPage = 1;
+            this.ItemsPerPage = this._list.Count;
+        }
+
+        public List<T> MoveToNextPage()
+        {
+            if (this._currentPage == this._pageCount)
+                return this.CurrentPageItems;
+
+            this._currentPage++;
+            this.UpdateCurrentPageItems();
+
+            return this.CurrentPageItems;
+        }
+
+        public List<T> MoveToPreviousPage()
+        {
+            if (this._currentPage == 1)
+                return this.CurrentPageItems;
+
+            this._currentPage--;
+            this.UpdateCurrentPageItems();
+
+            return this.CurrentPageItems;
+        }
+
+        public List<T> MoveToLastPage()
+        {
+            this._currentPage = this._pageCount;
+            this.UpdateCurrentPageItems();
+
+            return this.CurrentPageItems;
+        }
+
+        public List<T> MoveToFirstPage()
+        {
+            this._currentPage = 1;
+            this.UpdateCurrentPageItems();
+
+            return this.CurrentPageItems;
+        }
+
+        private void UpdateCurrentPageItems()
+        {
+            this._currentPageItems.Clear();
+            int startIndex = this.StartIndex;
+            int endIndex = this.EndIndex;
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                this._currentPageItems.Add(this._list[i]);
+            }
+        }
+
+        private void UpdatePageCount()
+        {
+            this._pageCount = (this._list.Count + this._itemsPerPage - 1) / this._itemsPerPage;
+        }
+    }
+}
---
+++ UnitTestPhuDinhCommon/UnitTestPagingCollection.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPhuDinhCommon
+{
+    [TestClass]
+    public class UnitTestPagingCollection
+    {
+        [TestMethod]
+        public void Test_Constructor()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList());
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 1, 1, 10 });
+        }
+
+        #region Test ItemPerPage
+        [TestMethod]
+        public void Test_ItemPerPage_ListItemCount_Is_Multiple_Of_ItemPerPage()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 2 };
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 5, 1, 2 });
+        }
+
+        [TestMethod]
+        public void Test_ItemPerPage_ListItemCount_Not_Multiple_Of_ItemPerPage()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 4, 1, 3 });
+        }
+
+        [TestMethod]
+        public void Test_ItemPerPage_ItemPerPage_Less_Than_one()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 0 };
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 10, 1, 1 });
+        }
+        #endregion
+
+        #region Test CurrentPage
+        [TestMethod]
+        public void Test_CurrentPage_With_CurrentPage_Less_Than_One()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 0;
+
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 4, 1, 3 });
+            CollectionAssert.AreEqual(pagedCollection.CurrentPageItems, new List<int> { 1, 2, 3 });
+        }
+
+        public void Test_CurrentPage_With_CurrentPage_Greater_Than_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = pagedCollection.PageCount + 1;
+
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 4, 4, 3 });
+            CollectionAssert.AreEqual(pagedCollection.CurrentPageItems, new List<int> { 10 });
+        }
+
+        [TestMethod]
+        public void Test_CurrentPage_With_CurrentPage_Between_One_And_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 3;
+
+            var result = new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage };
+            CollectionAssert.AreEqual(result, new List<int> { 4, 3, 3 });
+            CollectionAssert.AreEqual(pagedCollection.CurrentPageItems, new List<int> { 7, 8, 9 });
+        }
+        #endregion
+
+        #region Test MoveToNextPage
+        [TestMethod]
+        public void Test_MoveToNextPage_With_CurrentPage_Equal_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = pagedCollection.PageCount;
+
+            var result = pagedCollection.MoveToNextPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 4, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 10 });
+        }
+
+        [TestMethod]
+        public void Test_MoveToNextPage_With_CurrentPage_Between_One_And_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            var result = pagedCollection.MoveToNextPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 2, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 4, 5, 6 });
+        }
+
+        [TestMethod]
+        public void Test_MoveToNextPage_With_CurrentPage_Less_Than_One()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 0;
+
+            var result = pagedCollection.MoveToNextPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 2, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 4, 5, 6 });
+        }
+        #endregion
+
+        #region Test MoveToPrevious
+        [TestMethod]
+        public void Test_MoveToPreviousPage_With_CurrentPage_Equal_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = pagedCollection.PageCount;
+
+            var result = pagedCollection.MoveToPreviousPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 3, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 7, 8, 9 });
+        }
+
+        [TestMethod]
+        public void Test_MoveToPreviousPage_With_CurrentPage_Between_One_And_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 2;
+
+            var result = pagedCollection.MoveToPreviousPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 1, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 1, 2, 3 });
+        }
+
+        [TestMethod]
+        public void Test_MoveToPreviousPage_With_CurrentPage_Less_Than_One()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 0;
+
+            var result = pagedCollection.MoveToPreviousPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 1, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 1, 2, 3 });
+        }
+        #endregion
+
+        #region Test MoveToFirstPage
+        [TestMethod]
+        public void Test_MoveToFirstPage_With_CurrentPage_Less_Than_One()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 0;
+
+            var result = pagedCollection.MoveToFirstPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 1, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 1, 2, 3 });
+        }
+
+        [TestMethod]
+        public void Test_MoveToFirstPage_With_CurrentPage_Greater_Than_One()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 3;
+
+            var result = pagedCollection.MoveToFirstPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 1, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 1, 2, 3 });
+        }
+        #endregion
+
+        #region Test MoveToLastPage
+        [TestMethod]
+        public void Test_MoveToLastPage_With_CurrentPage_Less_Than_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = 0;
+
+            var result = pagedCollection.MoveToLastPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 4, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 10 });
+        }
+
+        [TestMethod]
+        public void Test_MoveToLastPage_With_CurrentPage_Greater_Than_PageCount()
+        {
+            var pagedCollection = new PhuDinhCommon.PagingCollection<int>(this.GetIntList()) { ItemsPerPage = 3 };
+
+            pagedCollection.CurrentPage = pagedCollection.PageCount + 1;
+
+            var result = pagedCollection.MoveToLastPage();
+
+            CollectionAssert.AreEqual(new List<int> { pagedCollection.PageCount, pagedCollection.CurrentPage, pagedCollection.ItemsPerPage }
+                , new List<int> { 4, 4, 3 });
+            CollectionAssert.AreEqual(result, new List<int> { 10 });
+        }
+        #endregion
+
+        private List<int> GetIntList()
+        {
+            return new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+    }
+}
