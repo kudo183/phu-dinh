300e05d7be78a7c2798d5033e08e6aabfb688ac9
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-05-31 08:41:37 +0000

    - update rKhachHang, rDiaDiem. - add rNuoc.

--- Database/dbo/Tables/rDiaDiem.sql
+++ Database/dbo/Tables/rDiaDiem.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[rDiaDiem] (
-    [Ma]   INT           NOT NULL,
-    [Nuoc] NVARCHAR (50) NOT NULL,
-    [Mien] NVARCHAR (50) NOT NULL,
-    [Tinh] NVARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_rDiaDiem] PRIMARY KEY CLUSTERED ([Ma] ASC)
+    [Ma]     INT            NOT NULL,
+    [MaNuoc] INT            NOT NULL,
+    [Tinh]   NVARCHAR (200) NOT NULL,
+    CONSTRAINT [PK_rDiaDiem] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [FK_rDiaDiem_rNuoc] FOREIGN KEY ([MaNuoc]) REFERENCES [dbo].[rNuoc] ([Ma])
 );
+
+
 
--- Database/dbo/Tables/rKhachHang.sql
+++ Database/dbo/Tables/rKhachHang.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE [dbo].[rKhachHang] (
     [Ma]           INT            IDENTITY (1, 1) NOT NULL,
+    [MaDiaDiem]    INT            NOT NULL,
     [TenKhachHang] NVARCHAR (200) NOT NULL,
-    CONSTRAINT [PK_rKhachHang] PRIMARY KEY CLUSTERED ([Ma] ASC)
+    CONSTRAINT [PK_rKhachHang] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [FK_rKhachHang_rDiaDiem] FOREIGN KEY ([MaDiaDiem]) REFERENCES [dbo].[rDiaDiem] ([Ma])
 );
+
+
 
