6005d473e49d0c695edec3380e82ec72c31563c0
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-05-31 08:11:47 +0000

    - update database

---
+++ Database/dbo/Stored Procedures/clean_database.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE clean_database
+as
+BEGIN
+	delete from dbo.tChiTietDonHang
+	delete from dbo.tDonHang
+	delete from dbo.tChuyenHang
+	delete from dbo.tChiPhiNhanVienGiaoHang
+	delete from dbo.rNhanVienGiaoHang
+	delete from dbo.tMatHang
+	delete from dbo.rKhachHang
+	delete from dbo.rPhuongTien
+	delete from dbo.rLoaiHang
+	delete from dbo.rLoaiChiPhi
+	delete from dbo.rDiaDiem
+	delete from dbo.rChanh
+	delete from dbo.rBaiXe
+END
--- Database/dbo/Tables/rChanh.sql
+++ Database/dbo/Tables/rChanh.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE [dbo].[rChanh] (
     [Ma]       INT            IDENTITY (1, 1) NOT NULL,
+    [MaBaiXe]  INT            NOT NULL,
     [TenChanh] NVARCHAR (200) NOT NULL,
-    CONSTRAINT [PK_rChanh] PRIMARY KEY CLUSTERED ([Ma] ASC)
+    CONSTRAINT [PK_rChanh] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [FK_rChanh_rBaiXe] FOREIGN KEY ([MaBaiXe]) REFERENCES [dbo].[rBaiXe] ([Ma])
 );
+
+
 
