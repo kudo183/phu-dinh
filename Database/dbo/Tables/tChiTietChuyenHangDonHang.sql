c38c5fdc01d21e8d29fda8c2b6184e9eae3dbedd
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-01 08:29:21 +0000

    - update tChiTietChuyenHangDonHang primary key.

--- Database/dbo/Tables/tChiTietChuyenHangDonHang.sql
+++ Database/dbo/Tables/tChiTietChuyenHangDonHang.sql
@@ -3,10 +3,12 @@
     [MaChuyenHangDonHang] INT NOT NULL,
     [MaMatHang]           INT NOT NULL,
     [SoLuong]             INT NOT NULL,
-    CONSTRAINT [PK_tChiTietChuyenHangMatHang] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [PK_tChiTietChuyenHangDonHang] PRIMARY KEY CLUSTERED ([Ma] ASC),
     CONSTRAINT [FK_tChiTietChuyenHangDonHang_tChuyenHangDonHang] FOREIGN KEY ([MaChuyenHangDonHang]) REFERENCES [dbo].[tChuyenHangDonHang] ([Ma]),
     CONSTRAINT [FK_tChiTietChuyenHangDonHang_tMatHang] FOREIGN KEY ([MaMatHang]) REFERENCES [dbo].[tMatHang] ([Ma])
 );
+
+
 
 
 
