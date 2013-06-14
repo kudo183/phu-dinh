fb098026d4e544df7484a5197975bf2f2bae9828
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-14 07:40:13 +0000

    - create DataGridReadOnlyTextColumn: Text, ReadOnly, IsTapStop = False. - change all Ma column to use DataGridReadOnlyTextColumn.

---
+++ PhuDinhCommonControl/CustomControl/DataGridReadOnlyTextColumn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridReadOnlyTextColumn.xaml
+    /// </summary>
+    public partial class DataGridReadOnlyTextColumn : DataGridTextColumn
+    {
+        public DataGridReadOnlyTextColumn()
+        {
+            InitializeComponent();
+        }
+    }
+}
