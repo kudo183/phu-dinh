74a68bf7667210ea707c9cade88ebeadeb0f2f26
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-15 11:05:32 +0000

    - add DataGridComboBoxColumn. - change all datagrid to use DataGridComboBoxColumnExt instead of DataGridTemplateColumn. - update DataGridExt for active DataGridComboBoxColumnExt.

---
+++ PhuDinhCommonControl/CustomControl/DataGridComboBoxColumnExt.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridComboBoxColumnExt.xaml
+    /// </summary>
+    public partial class DataGridComboBoxColumnExt : DataGridComboBoxColumn
+    {
+        public DataGridComboBoxColumnExt()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- PhuDinhCommonControl/CustomControl/DataGridExt.cs
+++ PhuDinhCommonControl/CustomControl/DataGridExt.cs
@@ -9,28 +9,50 @@ namespace PhuDinhCommonControl.CustomControl
             base.OnPreparingCellForEdit(e);
             var count = System.Windows.Media.VisualTreeHelper.GetChildrenCount(e.EditingElement);
 
+            var editingElementType = e.EditingElement.GetType();
+            if (editingElementType == typeof(ComboBox))
+            {
+                ActiveComboBox(e.EditingElement as ComboBox);
+                return;
+            }
+
             for (int i = 0; i < count; i++)
             {
                 var element = System.Windows.Media.VisualTreeHelper.GetChild(e.EditingElement, i);
                 var type = element.GetType();
                 if (type == typeof(DatePicker))
                 {
-                    (element as DatePicker).Focus();
+                    ActiveDatePicker(element as DatePicker);
                 }
                 else if (type == typeof(ComboBox))
                 {
-                    var combo = (element as ComboBox);
-                    var txt = VisualTreeUtils.FindChild<TextBox>(combo, "PART_EditableTextBox");
-                    System.Windows.Input.Keyboard.Focus(txt);
-                    txt.SelectAll();
-                    combo.IsDropDownOpen = true;
+                    ActiveComboBox(element as ComboBox);
                 }
-                else if (type == typeof(PhuDinhCommonControl.CustomControl.TimeInput))
+                else if (type == typeof(TimeInput))
                 {
-                    var timeInput = (element as PhuDinhCommonControl.CustomControl.TimeInput);
-                    System.Windows.Input.Keyboard.Focus(timeInput.txtHour);
+                    ActiveTimeInput(element as TimeInput);
                 }
             }
+        }
+
+        private void ActiveComboBox(ComboBox combo)
+        {
+            var txt = VisualTreeUtils.FindChild<TextBox>(combo, "PART_EditableTextBox");
+            System.Windows.Input.Keyboard.Focus(txt);
+            txt.SelectAll();
+            combo.IsDropDownOpen = true;
+        }
+
+        private void ActiveTimeInput(TimeInput timeInput)
+        {
+            System.Windows.Input.Keyboard.Focus(timeInput.txtHour);
+        }
+
+        private void ActiveDatePicker(DatePicker datePicker)
+        {
+            var txt = VisualTreeUtils.FindChild<TextBox>(datePicker, "PART_TextBox");
+            System.Windows.Input.Keyboard.Focus(txt);
+            txt.Select(0, 2);
         }
     }
 }
