50c2e90693e4676286fb4c493f8c9357cddeaaad
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-16 17:53:26 +0000

    - format code.

--- PhuDinhData/DesignTimeData/DDrBaiXe.cs
+++ PhuDinhData/DesignTimeData/DDrBaiXe.cs
@@ -5,17 +5,17 @@ namespace PhuDinhData.DesignTimeData
     public class DDrBaiXe
     {
         public static List<rBaiXe> rBaiXes = new List<rBaiXe>()
-                                                 {
-                                                     new rBaiXe(){Ma = 1, DiaDiemBaiXe = "Bai xe 1"},
-                                                     new rBaiXe(){Ma = 2, DiaDiemBaiXe = "Bai xe 2"},
-                                                     new rBaiXe(){Ma = 3, DiaDiemBaiXe = "Bai xe 3"},
-                                                     new rBaiXe(){Ma = 4, DiaDiemBaiXe = "Bai xe 4"},
-                                                     new rBaiXe(){Ma = 5, DiaDiemBaiXe = "Bai xe 5"},
-                                                     new rBaiXe(){Ma = 6, DiaDiemBaiXe = "Bai xe 6"},
-                                                     new rBaiXe(){Ma = 7, DiaDiemBaiXe = "Bai xe 7"},
-                                                     new rBaiXe(){Ma = 8, DiaDiemBaiXe = "Bai xe 8"},
-                                                     new rBaiXe(){Ma = 9, DiaDiemBaiXe = "Bai xe 9"},
-                                                     new rBaiXe(){Ma = 10, DiaDiemBaiXe = "Bai xe 10"},
-                                                 };
+        {
+            new rBaiXe(){Ma = 1, DiaDiemBaiXe = "Bai xe 1"},
+            new rBaiXe(){Ma = 2, DiaDiemBaiXe = "Bai xe 2"},
+            new rBaiXe(){Ma = 3, DiaDiemBaiXe = "Bai xe 3"},
+            new rBaiXe(){Ma = 4, DiaDiemBaiXe = "Bai xe 4"},
+            new rBaiXe(){Ma = 5, DiaDiemBaiXe = "Bai xe 5"},
+            new rBaiXe(){Ma = 6, DiaDiemBaiXe = "Bai xe 6"},
+            new rBaiXe(){Ma = 7, DiaDiemBaiXe = "Bai xe 7"},
+            new rBaiXe(){Ma = 8, DiaDiemBaiXe = "Bai xe 8"},
+            new rBaiXe(){Ma = 9, DiaDiemBaiXe = "Bai xe 9"},
+            new rBaiXe(){Ma = 10, DiaDiemBaiXe = "Bai xe 10"},
+        };
     }
 }
