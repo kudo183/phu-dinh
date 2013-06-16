641ebf77bff9de3641037e34ce0ef5dd6b7442e2
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-16 17:44:36 +0000

    * fixbug combobox not bind ItemSource.

--- PhuDinhCommonControl/CustomControl/DataGridColumn/DataGridComboBoxColumnExt.xaml.cs
+++ PhuDinhCommonControl/CustomControl/DataGridColumn/DataGridComboBoxColumnExt.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace PhuDinhCommonControl.CustomControl
 {
@@ -10,6 +12,31 @@ namespace PhuDinhCommonControl.CustomControl
         public DataGridComboBoxColumnExt()
         {
             InitializeComponent();
+        }
+
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            FrameworkElement element = base.GenerateEditingElement(cell, dataItem);
+            CopyItemsSource(element);
+            return element;
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            FrameworkElement element = base.GenerateElement(cell, dataItem);
+            CopyItemsSource(element);
+            return element;
+        }
+
+        private void CopyItemsSource(FrameworkElement element)
+        {
+            var binding = BindingOperations.GetBinding(this, ComboBox.ItemsSourceProperty);
+            if (binding == null)
+            {
+                return;
+            }
+
+            BindingOperations.SetBinding(element, ComboBox.ItemsSourceProperty, binding);
         }
     }
 }
