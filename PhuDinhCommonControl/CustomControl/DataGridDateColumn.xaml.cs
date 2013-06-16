a24ccddd50449d11b72a09b989abf5ed5d096a43
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-16 06:38:19 +0000

    - create DataGridDateColumn. - change all DataGridTemplate date column to DataGridDateColumn.

---
+++ PhuDinhCommonControl/CustomControl/DataGridDateColumn.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PhuDinhCommonControl.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridDateColumn.xaml
+    /// </summary>
+    public partial class DataGridDateColumn : DataGridBoundColumn
+    {
+        public DataGridDateColumn()
+        {
+            InitializeComponent();
+            FontSize = 16;
+        }
+
+        public double FontSize { get; set; }
+
+        protected override void CancelCellEdit(FrameworkElement editingElement, object uneditedValue)
+        {
+            var dp = editingElement as DatePicker;
+            if (dp != null)
+            {
+                dp.SelectedDate = DateTime.Parse(uneditedValue.ToString());
+            }
+        }
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            var dp = new DatePicker { FontSize = FontSize };
+            dp.SetBinding(DatePicker.SelectedDateProperty, Binding);
+
+            return dp;
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            var txt = new TextBlock { FontSize = FontSize };
+            txt.SetBinding(TextBlock.TextProperty, Binding);
+
+            return txt;
+        }
+
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+            var dp = editingElement as DatePicker;
+            if (dp != null)
+            {
+                var dt = dp.SelectedDate;
+                if (dt.HasValue)
+                    return dt.Value;
+            }
+            return DateTime.Today;
+        }
+
+        public class DateTimeConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                var date = (DateTime)value;
+
+                return date.ToString(parameter.ToString());
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                var strValue = value.ToString();
+                DateTime resultDateTime;
+
+                return DateTime.TryParse(strValue, out resultDateTime) ? resultDateTime : value;
+            }
+        }
+    }
+}
