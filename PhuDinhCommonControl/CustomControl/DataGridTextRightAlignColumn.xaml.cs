28c461b86000a2ce326eb7b7ae8bef58845fe93c
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-16 05:39:52 +0000

    - create DataGridRightAlignColumn. - change all datagrid number column to use DataGridRightAlignColumn.

---
+++ PhuDinhCommonControl/CustomControl/DataGridTextRightAlignColumn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridTextRightAlignColumn.xaml
+    /// </summary>
+    public partial class DataGridTextRightAlignColumn : DataGridTextColumn
+    {
+        public DataGridTextRightAlignColumn()
+        {
+            InitializeComponent();
+        }
+    }
+}
