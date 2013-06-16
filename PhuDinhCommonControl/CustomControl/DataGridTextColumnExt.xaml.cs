59a98f34d04f7e9a99f6aae3cdaa7996d576fca1
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-16 05:56:00 +0000

    - create DataGridTextColumnExt. - change all DataGridTextColumn to DataGridTextColumnExt.

---
+++ PhuDinhCommonControl/CustomControl/DataGridTextColumnExt.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridTextColumnExt.xaml
+    /// </summary>
+    public partial class DataGridTextColumnExt : DataGridTextColumn
+    {
+        public DataGridTextColumnExt()
+        {
+            InitializeComponent();
+        }
+    }
+}
