89ea6c64dee426b45d56cfb28c5caaac97e62b7c
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-06-21 11:13:18 +0000

    - handle Add button click for reference level 5.

--- PhuDinhCommonControl/View/5Level/tChiTietChuyenHangDonHangView.xaml.cs
+++ PhuDinhCommonControl/View/5Level/tChiTietChuyenHangDonHangView.xaml.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace PhuDinhCommonControl
 {
@@ -18,6 +19,7 @@ namespace PhuDinhCommonControl
         public Expression<Func<PhuDinhData.tChuyenHangDonHang, bool>> FilterChuyenHangDonHang { get; set; }
         public Expression<Func<PhuDinhData.tMatHang, bool>> FilterMatHang { get; set; }
 
+        private List<PhuDinhData.tChiTietChuyenHangDonHang> _tChiTietChuyenHangDonHangs;
         private List<PhuDinhData.tChuyenHangDonHang> _tChuyenHangDonHangs;
         private List<PhuDinhData.tMatHang> _tMatHangs;
         private readonly PhuDinhData.PhuDinhEntities _context = new PhuDinhData.PhuDinhEntities();
@@ -35,7 +37,7 @@ namespace PhuDinhCommonControl
         {
             try
             {
-                var data = this.dgChiTietChuyenHangDonHang.DataContext as ObservableCollection<PhuDinhData.tChiTietChuyenHangDonHang>;
+                var data = dgChiTietChuyenHangDonHang.DataContext as ObservableCollection<PhuDinhData.tChiTietChuyenHangDonHang>;
                 PhuDinhData.Repository.tChiTietChuyenHangDonHangRepository.Save(_context, data.ToList(), FilterChiTietChuyenHangDonHang);
                 RefreshView();
             }
@@ -52,22 +54,15 @@ namespace PhuDinhCommonControl
 
         public override void RefreshView()
         {
-            _tChuyenHangDonHangs = PhuDinhData.Repository.tChuyenHangDonHangRepository.GetData(_context, FilterChuyenHangDonHang);
-            _tMatHangs = PhuDinhData.Repository.tMatHangRepository.GetData(_context, FilterMatHang);
-
-            var data = PhuDinhData.Repository.tChiTietChuyenHangDonHangRepository.GetData(_context, FilterChiTietChuyenHangDonHang);
-
-            foreach (var tChiTietChuyenHangDonHang in data)
-            {
-                tChiTietChuyenHangDonHang.tChuyenHangDonHangList = _tChuyenHangDonHangs;
-                tChiTietChuyenHangDonHang.tMatHangList = _tMatHangs;
-            }
-
-            var collection = new ObservableCollection<PhuDinhData.tChiTietChuyenHangDonHang>(data);
+            _tChiTietChuyenHangDonHangs = PhuDinhData.Repository.tChiTietChuyenHangDonHangRepository.GetData(_context, FilterChiTietChuyenHangDonHang);
+            var collection = new ObservableCollection<PhuDinhData.tChiTietChuyenHangDonHang>(_tChiTietChuyenHangDonHangs);
             collection.CollectionChanged += collection_CollectionChanged;
-            this.dgChiTietChuyenHangDonHang.DataContext = collection;
+            dgChiTietChuyenHangDonHang.DataContext = collection;
 
-            this.dgChiTietChuyenHangDonHang.UpdateLayout();
+            UpdateMatHangReferenceData();
+            UpdateChuyenHangDonHangReferenceData();
+
+            dgChiTietChuyenHangDonHang.UpdateLayout();
         }
 
         void collection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -80,5 +75,51 @@ namespace PhuDinhCommonControl
             }
         }
         #endregion
+
+        private void dgChiTietChuyenHangDonHang_HeaderAddButtonClick(object sender, EventArgs e)
+        {
+            var header = sender as DataGridColumnHeader;
+
+            BaseView view = null;
+            Window w = null;
+
+            switch (header.Content.ToString())
+            {
+                case "Chuyến hàng đơn hàng":
+                    view = new tChuyenHangDonHangView();
+                    view.RefreshView();
+                    w = new Window { Title = "Chuyến hàng đơn hàng", Content = view };
+                    w.ShowDialog();
+
+                    UpdateChuyenHangDonHangReferenceData();
+                    break;
+                case "Mặt Hàng":
+                    view = new tMatHangView();
+                    view.RefreshView();
+                    w = new Window { Title = "Mặt Hàng", Content = view };
+                    w.ShowDialog();
+
+                    UpdateMatHangReferenceData();
+                    break;
+            }
+        }
+
+        private void UpdateChuyenHangDonHangReferenceData()
+        {
+            _tChuyenHangDonHangs = PhuDinhData.Repository.tChuyenHangDonHangRepository.GetData(_context, FilterChuyenHangDonHang);
+            foreach (var tChiTietChuyenHangDonHang in _tChiTietChuyenHangDonHangs)
+            {
+                tChiTietChuyenHangDonHang.tChuyenHangDonHangList = _tChuyenHangDonHangs;
+            }
+        }
+
+        private void UpdateMatHangReferenceData()
+        {
+            _tMatHangs = PhuDinhData.Repository.tMatHangRepository.GetData(_context, FilterMatHang);
+            foreach (var tChiTietChuyenHangDonHang in _tChiTietChuyenHangDonHangs)
+            {
+                tChiTietChuyenHangDonHang.tMatHangList = _tMatHangs;
+            }
+        }
     }
 }
