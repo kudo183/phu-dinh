6b43114f9a37191bb9dfa1e792fa45709ead4aff
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-07-05 09:37:18 +0000

    - add reseed command to clean_database producer.

--- Database/dbo/Stored Procedures/clean_database.sql
+++ Database/dbo/Stored Procedures/clean_database.sql
@@ -21,4 +21,20 @@ BEGIN
 	delete from dbo.rDiaDiem
 	delete from dbo.rChanh
 	delete from dbo.rBaiXe
+	
+	dbcc checkident (tChiTietChuyenHangDonHang, reseed, 0)
+	dbcc checkident (tChuyenHangDonHang, reseed, 0)
+	dbcc checkident (tChiTietDonHang, reseed, 0)
+	dbcc checkident (tDonHang, reseed, 0)
+	dbcc checkident (tChuyenHang, reseed, 0)
+	dbcc checkident (tChiPhiNhanVienGiaoHang, reseed, 0)
+	dbcc checkident (rNhanVienGiaoHang, reseed, 0)
+	dbcc checkident (tMatHang, reseed, 0)
+	dbcc checkident (rKhachHang, reseed, 0)
+	dbcc checkident (rPhuongTien, reseed, 0)
+	dbcc checkident (rLoaiHang, reseed, 0)
+	dbcc checkident (rLoaiChiPhi, reseed, 0)
+	dbcc checkident (rDiaDiem, reseed, 0)
+	dbcc checkident (rChanh, reseed, 0)
+	dbcc checkident (rBaiXe, reseed, 0)
 END
