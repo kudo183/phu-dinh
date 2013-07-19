161a7e924282257febd850ddb6ae0a087f361bb9
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-07-19 14:45:58 +0000

    - Add DataGridCheckBoxColumn. - Add Xong Column in DGDonHang.xaml, DGChiTietDonHang.xaml, DGChiTietChuyenHangDonHang.xaml, DGChuyenHangDonHang.xaml.

---
+++ PhuDinhCommonControl/CustomControl/DataGridColumn/DataGridCheckBoxColumnExt.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridCheckBoxColumnExt.xaml
+    /// </summary>
+    public partial class DataGridCheckBoxColumnExt : DataGridCheckBoxColumn
+    {
+        public DataGridCheckBoxColumnExt()
+        {
+            InitializeComponent();
+        }
+    }
+}
