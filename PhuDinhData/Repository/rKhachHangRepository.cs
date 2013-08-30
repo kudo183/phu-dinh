67d6de71b0cdcfeed7ce405aeee2e80eda2da03d
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-08-30 11:20:58 +0000

    - order by TenKhachHang.

--- PhuDinhData/Repository/rKhachHangRepository.cs
+++ PhuDinhData/Repository/rKhachHangRepository.cs
@@ -9,7 +9,7 @@ namespace PhuDinhData.Repository
     {
         public static List<rKhachHang> GetData(PhuDinhEntities context, Expression<Func<rKhachHang, bool>> filter)
         {
-            return Repository<rKhachHang>.GetData(context, filter).ToList();
+            return Repository<rKhachHang>.GetData(context, filter).OrderBy(p => p.TenKhachHang).ToList();
         }
 
         public static void Save(PhuDinhEntities context, List<rKhachHang> data, Expression<Func<rKhachHang, bool>> filter)
