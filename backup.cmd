75d165b7a1a5ce0393e8d74746f18cf230125726
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-09-28 18:44:26 +0000

    - add password when rar.

--- backup.cmd
+++ backup.cmd
@@ -5,5 +5,5 @@ set Rar="C:\Program Files\WinRAR\rar.exe"
 set OutputName="%d% %t%.bak"
 sqlcmd -s (local) -i backup_phudinh_db.sql
 rename "phudinh.bak" %OutputName%
-%Rar% a %OutputName%.rar %OutputName%
+%Rar% a -hpnobita %OutputName%.rar %OutputName%
 del %OutputName%
