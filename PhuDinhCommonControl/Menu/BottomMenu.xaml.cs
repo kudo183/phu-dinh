12bc34c2346e81fba211205b9121ca7b52c6eedf
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-09-28 18:31:49 +0000

    - remove unused Menu.

---
+++ PhuDinhCommonControl/Menu/BottomMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl.Menu
+{
+    /// <summary>
+    /// Interaction logic for BottomMenu.xaml
+    /// </summary>
+    public partial class BottomMenu : UserControl
+    {
+        public BottomMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
