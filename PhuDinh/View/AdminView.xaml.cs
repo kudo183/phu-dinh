f654ff6b71092bb950d18ba307d63ede276c7415
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-09-28 18:19:44 +0000

    - change to use tabcontrol instead of custom Menu.

--- PhuDinh/View/AdminView.xaml.cs
+++ PhuDinh/View/AdminView.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using PhuDinhCommon;
-using PhuDinhCommonControl;
+﻿using System.Windows.Controls;
 
 namespace PhuDinh
 {
@@ -13,76 +10,6 @@ namespace PhuDinh
         public AdminView()
         {
             InitializeComponent();
-        }
-
-        private void _spMenu_MenuItemClick(object sender, RoutedEventArgs e)
-        {
-            var menu = sender as PhuDinhCommonControl.Menu.MainMenu;
-            BaseView view = CreateView(menu.SelectedItem);
-
-            _brdMain.Child = view;
-
-            if (view != null)
-            {
-                view.RefreshView();
-            }
-        }
-
-        private BaseView CreateView(Constant.MainMenuItems selectedView)
-        {
-            BaseView result = null;
-            switch (selectedView)
-            {
-                case Constant.MainMenuItems.rBaiXe:
-                    result = new rBaiXeView();
-                    break;
-                case Constant.MainMenuItems.rChanh:
-                    result = new rChanhView();
-                    break;
-                case Constant.MainMenuItems.rDiaDiem:
-                    result = new rDiaDiemView();
-                    break;
-                case Constant.MainMenuItems.rKhachHang:
-                    result = new rKhachHangView();
-                    break;
-                case Constant.MainMenuItems.rLoaiChiPhi:
-                    result = new rLoaiChiPhiView();
-                    break;
-                case Constant.MainMenuItems.rLoaiHang:
-                    result = new rLoaiHangView();
-                    break;
-                case Constant.MainMenuItems.rNhanVien:
-                    result = new rNhanVienView();
-                    break;
-                case Constant.MainMenuItems.rNuoc:
-                    result = new rNuocView();
-                    break;
-                case Constant.MainMenuItems.rPhuongTien:
-                    result = new rPhuongTienView();
-                    break;
-                case Constant.MainMenuItems.tChiPhi:
-                    result = new tChiPhiView();
-                    break;
-                case Constant.MainMenuItems.tChiTietChuyenHangDonHang:
-                    result = new tChiTietChuyenHangDonHangView();
-                    break;
-                case Constant.MainMenuItems.tChiTietDonHang:
-                    result = new tChiTietDonHangView();
-                    break;
-                case Constant.MainMenuItems.tChuyenHang:
-                    result = new tChuyenHangView();
-                    break;
-                case Constant.MainMenuItems.tChuyenHangDonHang:
-                    result = new tChuyenHangDonHangView();
-                    break;
-                case Constant.MainMenuItems.tDonHang:
-                    result = new tDonHangView();
-                    break;
-                case Constant.MainMenuItems.tMatHang:
-                    result = new tMatHangView();
-                    break;
-            }
-            return result;
         }
     }
 }
