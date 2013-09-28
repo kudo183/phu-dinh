c34873714873095e9f3ee35b392ab60d2be6518b
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-09-28 18:20:59 +0000

    - auto add chitietchuyenhangdonhang after save chuyenhangdonhang.

--- PhuDinhCommonControl/View/ComplexView/ChuyenHangView.xaml.cs
+++ PhuDinhCommonControl/View/ComplexView/ChuyenHangView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace PhuDinhCommonControl
 {
@@ -45,8 +47,32 @@ namespace PhuDinhCommonControl
         void _tChuyenHangDonHangView_AfterSave(object sender, System.EventArgs e)
         {
             var chuyenHangDonHang =
-                _tChuyenHangDonHangView.dgChuyenHangDonHang.SelectedItem as PhuDinhData.tChuyenHangDonHang;
+                _tChuyenHangDonHangView.dgChuyenHangDonHang.SelectedItem
+                as PhuDinhData.tChuyenHangDonHang;
+
             RefreshChiTietChuyenHangDonHang(chuyenHangDonHang);
+
+            if (chuyenHangDonHang.tChiTietChuyenHangDonHangs.Count > 0)
+            {
+                return;
+            }
+
+            var context =
+                _tChiTietChuyenHangDonHangView.dgChiTietChuyenHangDonHang.DataContext
+                as ObservableCollection<PhuDinhData.tChiTietChuyenHangDonHang>;
+
+            foreach (var tChiTietDonHang in chuyenHangDonHang.tDonHang.tChiTietDonHangs.Where(p => p.Xong == false))
+            {
+                var ct = new PhuDinhData.tChiTietChuyenHangDonHang
+                {
+                    MaChiTietDonHang = tChiTietDonHang.Ma,
+                    tChuyenHangDonHang = chuyenHangDonHang,
+                    tChiTietDonHang = tChiTietDonHang,
+                    SoLuong = tChiTietDonHang.SoLuong - tChiTietDonHang.tChiTietChuyenHangDonHangs.Sum(p => p.SoLuong)
+                };
+
+                context.Add(ct);
+            }
         }
 
         void _tChuyenHangView_AfterSave(object sender, System.EventArgs e)
