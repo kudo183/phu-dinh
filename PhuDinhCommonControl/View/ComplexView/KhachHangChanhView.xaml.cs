4fff5b315843cf231b5f45ffbc6542dda18428b0
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-10-02 10:29:50 +0000

    - add KhachHangChanhView.

---
+++ PhuDinhCommonControl/View/ComplexView/KhachHangChanhView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Controls;
+
+namespace PhuDinhCommonControl
+{
+    /// <summary>
+    /// Interaction logic for KhachHangChanhView.xaml
+    /// </summary>
+    public partial class KhachHangChanhView : BaseView
+    {
+        public KhachHangChanhView()
+        {
+            InitializeComponent();
+            _rKhachHangView.RefreshView();
+
+            _rKhachHangView.dgKhachHang.SelectionChanged += dgKhachHang_SelectionChanged;
+        }
+
+        void dgKhachHang_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender != e.OriginalSource)
+            {
+                return;
+            }
+
+            var rKhachHang = ((DataGrid)sender).SelectedItem as PhuDinhData.rKhachHang;
+            if (rKhachHang == null)
+            {
+                _rKhachHangChanhView.FilterKhachHangChanh = null;
+                _rKhachHangChanhView.RefreshView();
+                return;
+            }
+
+            _rKhachHangChanhView.FilterKhachHangChanh = (p => p.MaKhachHang == rKhachHang.Ma);
+            _rKhachHangChanhView.RefreshView();
+        }
+    }
+}
