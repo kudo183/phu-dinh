efc56d320c9418cedd6a9bf905d25822eb677554
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-11-01 10:27:40 +0000

    - add PhuDinhXBAP project for xbap.

---
+++ PhuDinhXBAP/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PhuDinhXBAP
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessLogin();
+        }
+
+        private void txtPassword_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                ProcessLogin();
+            }
+        }
+
+        private void ProcessLogin()
+        {
+            var hasher = new SHA256Managed();
+            var unhashedPassword = System.Text.Encoding.Unicode.GetBytes(txtPassword.Password);
+            var hashed = hasher.ComputeHash(unhashedPassword);
+            var base64 = System.Convert.ToBase64String(hashed);
+            if (string.Equals(base64, "zLhmNPCmBNoiyzeJ9TqQJn6EDxSY1eUp2oblFCuVOc8="))
+            {
+                mainContent.Visibility = Visibility.Visible;
+                loginSP.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
