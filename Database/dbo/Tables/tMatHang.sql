ea47dc71f8d5e7c082a0d0c7f22fd0e8435d699a
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-11-05 10:38:57 +0000

    - add column TenMatHangDayDu.

--- Database/dbo/Tables/tMatHang.sql
+++ Database/dbo/Tables/tMatHang.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[tMatHang] (
-    [Ma]         INT            IDENTITY (1, 1) NOT NULL,
-    [MaLoai]     INT            NOT NULL,
-    [TenMatHang] NVARCHAR (200) NOT NULL,
-    [SoKy]       INT            CONSTRAINT [DF_tMatHang_SoKy] DEFAULT ((0)) NOT NULL,
-    [SoMet]      INT            CONSTRAINT [DF_tMatHang_SoMet] DEFAULT ((0)) NOT NULL,
+    [Ma]              INT            IDENTITY (1, 1) NOT NULL,
+    [MaLoai]          INT            NOT NULL,
+    [TenMatHang]      NVARCHAR (200) NOT NULL,
+    [SoKy]            INT            CONSTRAINT [DF_tMatHang_SoKy] DEFAULT ((0)) NOT NULL,
+    [SoMet]           INT            CONSTRAINT [DF_tMatHang_SoMet] DEFAULT ((0)) NOT NULL,
+    [TenMatHangDayDu] NVARCHAR (200) CONSTRAINT [DF_tMatHang_TenMatHangDayDu] DEFAULT ('') NOT NULL,
     CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([Ma] ASC),
     CONSTRAINT [FK_tMatHang_rLoaiHang] FOREIGN KEY ([MaLoai]) REFERENCES [dbo].[rLoaiHang] ([Ma])
 );
+
+
 
 
 
