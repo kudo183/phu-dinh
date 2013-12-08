c56fbb0db99e4f60fb34fb8becc85b9ac0408e1d
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2013-12-08 09:01:48 +0000

    - add new Save, RemoveItem code. - Save will return list of ChangedItemData.

--- PhuDinhData/Repository/Repository.cs
+++ PhuDinhData/Repository/Repository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
 
 namespace PhuDinhData.Repository
 {
@@ -20,15 +22,20 @@ namespace PhuDinhData.Repository
             }
         }
 
-        private static void AddNewItem(PhuDinhEntities context, List<T> gridDataSource, Func<T, bool> CheckNewItemFunc)
+        private static List<T> AddNewItem(PhuDinhEntities context, List<T> gridDataSource, Func<T, bool> CheckNewItemFunc)
         {
+            var result = new List<T>();
+
             foreach (var item in gridDataSource)
             {
                 if (CheckNewItemFunc(item))
                 {
+                    result.Add(item);
                     context.Set<T>().Add(item);
                 }
             }
+
+            return result;
         }
 
         public static IQueryable<T> GetData(PhuDinhEntities context, Expression<Func<T, bool>> filter)
@@ -44,5 +51,88 @@ namespace PhuDinhData.Repository
             context.SaveChanges();
             return changed;
         }
+
+        #region new code
+        private static List<T> RemoveItem(PhuDinhEntities context, List<T> gridDataSource, List<T> origData, Func<T, T, bool> CompareFunc)
+        {
+            var result = new List<T>();
+
+            foreach (var item in origData)
+            {
+                var entity = gridDataSource.FirstOrDefault(p => CompareFunc(item, p));
+                if (entity == null)
+                {
+                    result.Add(item);
+                    context.Set<T>().Remove(item);
+                }
+            }
+
+            return result;
+        }
+
+        public static List<ChangedItemData> Save(PhuDinhEntities context, List<T> data, List<T> origData, Func<T, bool> CheckNewItemFunc, Func<T, T, bool> CompareFunc)
+        {
+            var removedItems = RemoveItem(context, data, origData, CompareFunc);
+            var addedItems = AddNewItem(context, data, CheckNewItemFunc);
+            var changed = context.ChangeTracker.Entries<T>().Where(p => p.State == EntityState.Modified).ToList();
+
+            var changedItems = new List<ChangedItemData>();
+            changedItems.AddRange(changed.Select(item => new ChangedItemData
+            {
+                State = EntityState.Modified,
+                OriginalValues = item.OriginalValues.Clone().ToObject() as T,
+                CurrentValues = item.CurrentValues.Clone().ToObject() as T
+            }));
+
+            changedItems.AddRange(removedItems.Select(item => new ChangedItemData
+            {
+                State = EntityState.Deleted,
+                OriginalValues = null,
+                CurrentValues = item
+            }));
+
+            context.SaveChanges();
+
+            changedItems.AddRange(addedItems.Select(item => new ChangedItemData
+            {
+                State = EntityState.Added,
+                OriginalValues = null,
+                CurrentValues = item
+            }));
+
+            return changedItems;
+        }
+
+        public static string BuildLogString(IEnumerable<ChangedItemData> changedItems)
+        {
+            var result = new StringBuilder();
+
+            foreach (var item in changedItems)
+            {
+                if (item.State == EntityState.Modified)
+                {
+                    result.AppendLine(item.State.ToString());
+                    result.AppendLine(item.OriginalValues.ToString());
+                    result.AppendLine(item.CurrentValues.ToString());
+                    result.AppendLine();
+                }
+                else
+                {
+                    result.AppendLine(item.State.ToString());
+                    result.AppendLine(item.CurrentValues.ToString());
+                    result.AppendLine();
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public class ChangedItemData
+        {
+            public EntityState State { get; set; }
+            public T OriginalValues { get; set; }
+            public T CurrentValues { get; set; }
+        }
+        #endregion
     }
 }
