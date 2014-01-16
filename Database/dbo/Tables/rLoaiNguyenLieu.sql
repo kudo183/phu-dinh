304366ec060d3a92df2e7f7990db191c919bf289
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-01-16 01:57:12 +0000

    - change nchar to nvarchar.

--- Database/dbo/Tables/rLoaiNguyenLieu.sql
+++ Database/dbo/Tables/rLoaiNguyenLieu.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[rLoaiNguyenLieu] (
-    [Ma]      INT         IDENTITY (1, 1) NOT NULL,
-    [TenLoai] NCHAR (100) NOT NULL,
+    [Ma]      INT            IDENTITY (1, 1) NOT NULL,
+    [TenLoai] NVARCHAR (100) NOT NULL,
     CONSTRAINT [PK_rLoaiNguyenLieu] PRIMARY KEY CLUSTERED ([Ma] ASC)
 );
+
+
 
--- Database/dbo/Tables/rNhaCungCap.sql
+++ Database/dbo/Tables/rNhaCungCap.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[rNhaCungCap] (
-    [Ma]            INT         IDENTITY (1, 1) NOT NULL,
-    [TenNhaCungCap] NCHAR (200) NOT NULL,
+    [Ma]            INT            IDENTITY (1, 1) NOT NULL,
+    [TenNhaCungCap] NVARCHAR (200) NOT NULL,
     CONSTRAINT [PK_NhaCungCap] PRIMARY KEY CLUSTERED ([Ma] ASC)
 );
+
+
 
