31c5547474f3d04d9542cdb080b70497cf89634c
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-01-29 03:27:05 +0000

    - restructure EntityExt, Repository: group by reference level.

---
+++ PhuDinhData/EntityExt/0Level/rKhoHangExt.cs
@@ -0,0 +1,10 @@
+﻿namespace PhuDinhData
+{
+    public partial class rKhoHang
+    {
+        public override string ToString()
+        {
+            return string.Format("[Ma {0}] [TenKho {1}] [TrangThai {2}]", Ma, TenKho, TrangThai);
+        }
+    }
+}
