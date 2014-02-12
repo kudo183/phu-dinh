88a270f491c8fe1d32c1078035a733e4ae57a8df
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-02-12 09:44:04 +0000

    - add column SoLuongCu in table tTonKho.

--- Database/dbo/Tables/tTonKho.sql
+++ Database/dbo/Tables/tTonKho.sql
@@ -4,8 +4,11 @@
     [MaMatHang] INT  NOT NULL,
     [Ngay]      DATE NOT NULL,
     [SoLuong]   INT  NOT NULL,
+    [SoLuongCu] INT  NOT NULL,
     CONSTRAINT [PK_tTonKho] PRIMARY KEY CLUSTERED ([Ma] ASC),
     CONSTRAINT [FK_tTonKho_rKhoHang] FOREIGN KEY ([MaKhoHang]) REFERENCES [dbo].[rKhoHang] ([Ma]),
     CONSTRAINT [FK_tTonKho_tMatHang] FOREIGN KEY ([MaMatHang]) REFERENCES [dbo].[tMatHang] ([Ma])
 );
+
+
 
