2295bfc535d4894069862178196c22ebad2caa1e
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-02-13 14:28:35 +0000

    - change main page from TabPage to ButtonPage.

---
+++ PhuDinh/ButtonPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PhuDinh
+{
+    /// <summary>
+    /// Interaction logic for ButtonPage.xaml
+    /// </summary>
+    public partial class ButtonPage : UserControl
+    {
+        public ButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (e.OriginalSource as Button);
+            if (button == null)
+            {
+                return;
+            }
+
+            var w = new Window
+            {
+                Title = button.Content.ToString(),
+                Content = button.Tag,
+                WindowState = WindowState.Maximized
+            };
+            w.Show();
+        }
+    }
+}
