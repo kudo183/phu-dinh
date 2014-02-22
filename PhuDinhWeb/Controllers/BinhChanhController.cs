9fff2a5307cd0b79ede2727c78655eb7e2da6c84
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-02-22 05:09:15 +0000

    - order by TenMatHangDayDu.

--- PhuDinhWeb/Controllers/BinhChanhController.cs
+++ PhuDinhWeb/Controllers/BinhChanhController.cs
@@ -12,7 +12,8 @@ namespace PhuDinhWeb.Controllers
             var context = ContextFactory.CreateContext();
             var now = DateTime.Now.Date;
 
-            return View(context.tTonKhoes.Where(p => p.MaKhoHang == 2 && p.Ngay == now).ToList());
+            return View(context.tTonKhoes.Where(p => p.MaKhoHang == 2 && p.Ngay == now)
+                .OrderBy(p => p.tMatHang.TenMatHangDayDu).ToList());
         }
     }
 }
--- PhuDinhWeb/Controllers/PhuDinhController.cs
+++ PhuDinhWeb/Controllers/PhuDinhController.cs
@@ -12,7 +12,8 @@ namespace PhuDinhWeb.Controllers
             var context = ContextFactory.CreateContext();
             var now = DateTime.Now.Date;
 
-            return View(context.tTonKhoes.Where(p => p.MaKhoHang == 1 && p.Ngay == now).ToList());
+            return View(context.tTonKhoes.Where(p => p.MaKhoHang == 1 && p.Ngay == now)
+                .OrderBy(p => p.tMatHang.TenMatHangDayDu).ToList());
         }
     }
 }
