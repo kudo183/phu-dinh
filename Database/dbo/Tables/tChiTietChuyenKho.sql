146524e5e093d0c8c1eba602343dc4087aaabb65
phamquochuy@gmail.com <phamquochuy@gmail.com@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-02-23 16:31:17 +0000

    - add table tChuyenKho, tChiTietChuyenKho.

---
+++ Database/dbo/Tables/tChiTietChuyenKho.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tChiTietChuyenKho] (
+    [Ma]          INT IDENTITY (1, 1) NOT NULL,
+    [MaChuyenKho] INT NOT NULL,
+    [MaMatHang]   INT NOT NULL,
+    [SoLuong]     INT NOT NULL,
+    CONSTRAINT [PK_tChiTietChuyenKho] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [FK_tChiTietChuyenKho_tChuyenKho] FOREIGN KEY ([MaChuyenKho]) REFERENCES [dbo].[tChuyenKho] ([Ma]),
+    CONSTRAINT [FK_tChiTietChuyenKho_tMatHang] FOREIGN KEY ([MaMatHang]) REFERENCES [dbo].[tMatHang] ([Ma])
+);
+
---
+++ Database/dbo/Tables/tChuyenKho.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tChuyenKho] (
+    [Ma]            INT  IDENTITY (1, 1) NOT NULL,
+    [MaNhanVien]    INT  NOT NULL,
+    [MaKhoHangXuat] INT  NOT NULL,
+    [MaKhoHangNhap] INT  NOT NULL,
+    [Ngay]          DATE NOT NULL,
+    CONSTRAINT [PK_tChuyenKho] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [FK_tChuyenKho_rKhoHangNhap] FOREIGN KEY ([MaKhoHangNhap]) REFERENCES [dbo].[rKhoHang] ([Ma]),
+    CONSTRAINT [FK_tChuyenKho_rKhoHangXuat] FOREIGN KEY ([MaKhoHangXuat]) REFERENCES [dbo].[rKhoHang] ([Ma]),
+    CONSTRAINT [FK_tChuyenKho_rNhanVien] FOREIGN KEY ([MaNhanVien]) REFERENCES [dbo].[rNhanVien] ([Ma])
+);
+
