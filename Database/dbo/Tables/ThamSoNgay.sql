290aa2202ab266a1706201475fc6e05f6381595f
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-02-25 03:51:28 +0000

    - add ThamSoNgay.sql for storing date parameter value.

---
+++ Database/dbo/Tables/ThamSoNgay.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ThamSoNgay] (
+    [Ten]    NVARCHAR (50) NOT NULL,
+    [GiaTri] DATE          NOT NULL,
+    CONSTRAINT [PK_ThamSoNgay] PRIMARY KEY CLUSTERED ([Ten] ASC)
+);
+
