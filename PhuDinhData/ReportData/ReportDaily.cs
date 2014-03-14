5e53aab6787de2c2267e5ed120b9e5543d1e69f5
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-03-14 10:50:58 +0000

    - add Function ReportDaily.

---
+++ PhuDinhData/ReportData/ReportDaily.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhuDinhData.ReportData
+{
+    public static class ReportDaily
+    {
+        public class ReportDailyData
+        {
+            public string TenKho { get; set; }
+            public string TenKhachHang { get; set; }
+            public string TenMatHang { get; set; }
+            public string SoLuong { get; set; }
+        }
+
+        public static List<ReportDailyData> FilterByDate(DateTime ngay)
+        {
+            return Filter(p => p.Ngay == ngay);
+        }
+
+        private static List<ReportDailyData> Filter(System.Linq.Expressions.Expression<Func<tDonHang, bool>> filter)
+        {
+            var context = ContextFactory.CreateContext();
+
+            var donHangs = context.tDonHangs
+                .Where(filter).GroupBy(p => p.MaKhoHang);
+
+            var result = new List<ReportDailyData>();
+
+            foreach (var donHang in donHangs)
+            {
+                result.Add(new ReportDailyData()
+                {
+                    TenKho = donHang.First().rKhoHang.TenKho
+                });
+                foreach (var tDonHang in donHang)
+                {
+                    result.Add(new ReportDailyData()
+                    {
+                        TenKhachHang = tDonHang.rKhachHang.TenKhachHang
+                    });
+                    foreach (var tChiTietDonHang in tDonHang.tChiTietDonHangs)
+                    {
+                        result.Add(new ReportDailyData()
+                        {
+                            TenMatHang = tChiTietDonHang.tMatHang.TenMatHangDayDu,
+                            SoLuong = tChiTietDonHang.SoLuong.ToString()
+                        });
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PhuDinhReport/ReportDailyView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using PhuDinhData.ReportData;
+using System;
+using System.Windows.Controls;
+
+namespace PhuDinhReport
+{
+    /// <summary>
+    /// Interaction logic for ReportDailyView.xaml
+    /// </summary>
+    public partial class ReportDailyView : UserControl
+    {
+        public ReportDailyView()
+        {
+            InitializeComponent();
+
+            var now = DateTime.Now;
+            dpNgay.SelectedDate = now;
+        }
+
+        private void btnNgay_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var ngay = dpNgay.SelectedDate.Value.Date;
+
+            dg.ItemsSource = ReportDaily.FilterByDate(ngay);
+        }
+    }
+}
