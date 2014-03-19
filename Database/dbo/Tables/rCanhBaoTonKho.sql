4785870bf8fc8a4e008430bce5eb4c959016e86e
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-03-19 07:55:44 +0000

    - add column KhachRieng - add table rCanhBaoTonKho

---
+++ Database/dbo/Tables/rCanhBaoTonKho.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[rCanhBaoTonKho] (
+    [Ma]          INT IDENTITY (1, 1) NOT NULL,
+    [MaMatHang]   INT NOT NULL,
+    [MaKhoHang]   INT NOT NULL,
+    [TonCaoNhat]  INT NOT NULL,
+    [TonThapNhat] INT NOT NULL,
+    CONSTRAINT [PK_rCanhBaoTonKho] PRIMARY KEY CLUSTERED ([Ma] ASC),
+    CONSTRAINT [FK_rCanhBaoTonKho_rKhoHang] FOREIGN KEY ([MaKhoHang]) REFERENCES [dbo].[rKhoHang] ([Ma]),
+    CONSTRAINT [FK_rCanhBaoTonKho_tMatHang] FOREIGN KEY ([MaMatHang]) REFERENCES [dbo].[tMatHang] ([Ma])
+);
+
--- Database/dbo/Tables/rKhachHang.sql
+++ Database/dbo/Tables/rKhachHang.sql
@@ -2,9 +2,12 @@
     [Ma]           INT            IDENTITY (1, 1) NOT NULL,
     [MaDiaDiem]    INT            NOT NULL,
     [TenKhachHang] NVARCHAR (200) NOT NULL,
+    [KhachRieng]   BIT            CONSTRAINT [DF_rKhachHang_KhachRieng] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_rKhachHang] PRIMARY KEY CLUSTERED ([Ma] ASC),
     CONSTRAINT [FK_rKhachHang_rDiaDiem] FOREIGN KEY ([MaDiaDiem]) REFERENCES [dbo].[rDiaDiem] ([Ma])
 );
+
+
 
 
 
