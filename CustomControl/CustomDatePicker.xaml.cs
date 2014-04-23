4d15bdf09c7e0328e8eaaef1405a78d8f15ab405
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-04-23 07:12:27 +0000

    - add CustomDatePicker control - apply CustomDatePicker to DataGridDateColumn.

---
+++ CustomControl/CustomDatePicker.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace CustomControl
+{
+    /// <summary>
+    /// Interaction logic for CustomDatePicker.xaml
+    /// </summary>
+    public partial class CustomDatePicker : UserControl
+    {
+        private Binding _selectedDateBinding;
+        public Binding SelectedDateBinding
+        {
+            get { return _selectedDateBinding; }
+            set
+            {
+                if (_selectedDateBinding == value)
+                {
+                    return;
+                }
+
+                _selectedDateBinding = value;
+                dp.SetBinding(DatePicker.SelectedDateProperty, _selectedDateBinding);
+            }
+        }
+
+        private Binding _isEnabledBinding;
+        public Binding IsEnabledBinding
+        {
+            get { return _isEnabledBinding; }
+            set
+            {
+                if (_isEnabledBinding == value)
+                {
+                    return;
+                }
+
+                _isEnabledBinding = value;
+                dp.SetBinding(IsEnabledProperty, _isEnabledBinding);
+            }
+        }
+
+        public CustomDatePicker()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrev_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (dp.SelectedDate == null)
+            {
+                return;
+            }
+
+            dp.SelectedDate = dp.SelectedDate.Value.Subtract(new TimeSpan(1, 0, 0, 0));
+        }
+
+        private void btnNext_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (dp.SelectedDate == null)
+            {
+                return;
+            }
+
+            dp.SelectedDate = dp.SelectedDate.Value.AddDays(1);
+        }
+    }
+}
