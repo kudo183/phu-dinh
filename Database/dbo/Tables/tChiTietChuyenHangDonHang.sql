0c4be781c6d22b00ac510c16846cd965bf0050b6
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-06-12 09:17:37 +0000

    - add trigger to update Xong.

--- Database/dbo/Tables/tChiTietChuyenHangDonHang.sql
+++ Database/dbo/Tables/tChiTietChuyenHangDonHang.sql
@@ -14,3 +14,72 @@
 
 
 
+
+
+
+GO
+create TRIGGER [dbo].[tChiTietChuyenHangDonHang_trUpdateXong]
+	ON [dbo].[tChiTietChuyenHangDonHang]
+	after DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+		Declare @Evt Int = 0;
+		Declare @SoLuong Int = 0;
+		Declare @SoLuongChiTietDonHang Int = 0;
+		Declare @MaDonHang Int = 0;
+		Declare @MaChiTietDonHang Int = 0;
+		Declare @Xong bit = 0;
+
+		SET NOCOUNT ON
+
+		IF(EXISTS(SELECT * FROM inserted))
+			Set @Evt = @Evt + 1;
+		IF(EXISTS(SELECT * FROM deleted))
+			Set @Evt = @Evt + 2;
+			
+		IF(@Evt = 1) -- inserted
+		begin
+			select @MaChiTietDonHang = MaChiTietDonHang
+			from inserted
+		end
+
+		if(@Evt = 2) -- deleted
+		begin
+			select @MaChiTietDonHang = MaChiTietDonHang
+			from deleted
+		end
+
+		if(@Evt = 3) -- updated
+		begin
+			select @MaChiTietDonHang = MaChiTietDonHang
+			from inserted
+		end
+		
+		select @SoLuongChiTietDonHang = SoLuong, @MaDonHang = MaDonHang
+		from [dbo].[tChiTietDonHang]
+		where Ma = @MaChiTietDonHang
+
+		select @SoLuong = sum(SoLuong)
+		from [dbo].[tChiTietChuyenHangDonHang]
+		where MaChiTietDonHang = @MaChiTietDonHang
+
+		set @Xong = 0;
+
+		if(@SoLuong = @SoLuongChiTietDonHang)
+			set @Xong = 1
+
+		begin		
+			update [dbo].[tChiTietDonHang]
+			set Xong = @Xong
+			where Ma = @MaChiTietDonHang
+
+			set @Xong = 1;
+
+			if(EXISTS(select * from [dbo].[tChiTietDonHang] where MaDonHang = @MaDonHang and Xong = 0))
+				set @Xong = 0;
+
+			update [dbo].[tDonHang]
+			set Xong = @Xong
+			where Ma = @MaDonHang
+		end
+	END
--- Database/dbo/Tables/tChiTietDonHang.sql
+++ Database/dbo/Tables/tChiTietDonHang.sql
@@ -11,3 +11,78 @@
 
 
 
+
+
+
+GO
+create TRIGGER [dbo].[tChiTietDonHang_trUpdateXong]
+	ON [dbo].[tChiTietDonHang]
+	after DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+		Declare @Evt Int = 0;
+		Declare @SoLuong Int = 0;
+		Declare @SoLuongChiTietDonHang Int = 0;
+		Declare @MaDonHang Int = 0;
+		Declare @MaChiTietDonHang Int = 0;
+		Declare @Xong bit = 0;
+
+		SET NOCOUNT ON
+
+		IF(EXISTS(SELECT * FROM inserted))
+			Set @Evt = @Evt + 1;
+		IF(EXISTS(SELECT * FROM deleted))
+			Set @Evt = @Evt + 2;
+			
+		IF(@Evt = 1) -- inserted
+		begin
+			select @MaDonHang = MaDonHang
+			from inserted
+
+			set @Xong = 0;
+		end
+
+		if(@Evt = 2) -- deleted
+		begin
+			select @MaDonHang = MaDonHang
+			from deleted
+			
+			set @Xong = 1;
+
+			if(EXISTS(select * from [dbo].[tChiTietDonHang] where MaDonHang = @MaDonHang and Xong = 0))
+				set @Xong = 0;
+		end
+
+		if(@Evt = 3) -- updated
+		begin
+			select @MaChiTietDonHang = Ma, @SoLuongChiTietDonHang = SoLuong, @MaDonHang = MaDonHang
+			from inserted
+				
+			if(EXISTS(select * from [dbo].[tChiTietChuyenHangDonHang] where MaChiTietDonHang = @MaChiTietDonHang))
+			begin
+				select @SoLuong = sum(SoLuong)
+				from [dbo].[tChiTietChuyenHangDonHang]
+				where MaChiTietDonHang = @MaChiTietDonHang
+
+				set @Xong = 0;
+
+				if(@SoLuong = @SoLuongChiTietDonHang)
+					set @Xong = 1;
+
+				update [dbo].[tChiTietDonHang]
+				set Xong = @Xong
+				where Ma = @MaChiTietDonHang
+								
+				set @Xong = 1;
+
+				if(EXISTS(select * from [dbo].[tChiTietDonHang] where MaDonHang = @MaDonHang and Xong = 0))
+					set @Xong = 0;
+			end
+		end
+		
+		begin		
+			update [dbo].[tDonHang]
+			set Xong = @Xong
+			where Ma = @MaDonHang
+		end
+	END
