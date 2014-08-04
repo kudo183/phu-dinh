d33e018301d616762926315b4cbef52bbb44c700
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-08-04 08:07:22 +0000

    - modify TextBoxExt to support EnterKeyPressBinding. - Use EnterKeyPressBinding in DataGridComboBoxColumnExt

--- CustomControl/TextBoxExt.cs
+++ CustomControl/TextBoxExt.cs
@@ -1,13 +1,28 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace CustomControl
 {
-    public class TextBoxExt:TextBox
+    public class TextBoxExt : TextBox
     {
-        protected override void OnGotKeyboardFocus(System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        public bool IsUseEnterKeyPressBinding { get; set; }
+
+        protected override void OnGotKeyboardFocus(KeyboardFocusChangedEventArgs e)
         {
             base.OnGotKeyboardFocus(e);
             SelectAll();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (IsUseEnterKeyPressBinding == true && e.Key == Key.Return)
+            {
+                var exp = GetBindingExpression(TextBox.TextProperty);
+
+                exp.UpdateSource();
+            }
         }
     }
 }
