fa22bf3aea7587ba849522972f33d013abcba199
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2014-12-04 07:44:41 +0000

    - apply ReportDatePicker to ReportMatHangView.

--- PhuDinhReport/ReportByMatHangView.xaml.cs
+++ PhuDinhReport/ReportByMatHangView.xaml.cs
@@ -14,44 +14,40 @@ namespace PhuDinhReport
         {
             InitializeComponent();
 
-            dpNgay.dp.SelectedDateChanged += dp_SelectedDateChanged;
-
-            var now = DateTime.Now;
-            dpNgay.dp.SelectedDate = now;
-            dpTuNgay.dp.SelectedDate = now;
-            dpDenNgay.dp.SelectedDate = now;
+            reportDatePicker.NgaySelected += reportDatePicker_NgaySelected;
+            reportDatePicker.TuNgayDenNgaySelected += reportDatePicker_TuNgayDenNgaySelected;
         }
 
-        void dp_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        void reportDatePicker_NgaySelected(object sender, EventArgs e)
         {
             ReportByDate();
         }
 
-        private void ReportByDate()
-        {
-            var ngay = dpNgay.dp.SelectedDate.Value.Date;
-
-            var result = ReportByMatHang.FilterByDate(ngay);
-
-            txtMsg.Text = result.Sum(p => p.SoLuong).ToString("N0");
-
-            dg.ItemsSource = result;
-        }
-
-        private void btnTuNgayDenNgay_Click(object sender, System.Windows.RoutedEventArgs e)
+        void reportDatePicker_TuNgayDenNgaySelected(object sender, EventArgs e)
         {
             ReportFromDateToDate();
         }
 
+        private void ReportByDate()
+        {
+            var ngay = reportDatePicker.Ngay.Value.Date;
+
+            var result = ReportByMatHang.FilterByDate(ngay);
+
+            reportDatePicker.NgayMsg = string.Format("Tong so cuon: {0}", result.Sum(p => p.SoLuong).ToString("N0"));
+
+            dg.ItemsSource = result;
+        }
+
         private void ReportFromDateToDate()
         {
-            var tuNgay = dpTuNgay.dp.SelectedDate.Value.Date;
+            var tuNgay = reportDatePicker.TuNgay.Value.Date;
 
-            var denNgay = dpDenNgay.dp.SelectedDate.Value.Date;
+            var denNgay = reportDatePicker.DenNgay.Value.Date;
 
             var result = ReportByMatHang.FilterByDate(tuNgay, denNgay);
 
-            txtMsg.Text = result.Sum(p => p.SoLuong).ToString("N0");
+            reportDatePicker.TuNgayDenNgayMsg = string.Format("Tong so cuon: {0}", result.Sum(p => p.SoLuong).ToString("N0"));
 
             dg.ItemsSource = result;
         }
