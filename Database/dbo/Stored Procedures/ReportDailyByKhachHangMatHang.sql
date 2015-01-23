629f16740770c2321d2183d3be0674a09a0fc256
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2015-01-23 11:39:15 +0000

    *fixbug: Sum (not GroupBy) - add order by kh.TenKhachHang

--- Database/dbo/Stored Procedures/ReportDailyByKhachHangMatHang.sql
+++ Database/dbo/Stored Procedures/ReportDailyByKhachHangMatHang.sql
@@ -3,16 +3,17 @@
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-create PROCEDURE [dbo].[ReportDailyByKhachHangMatHang] 
+CREATE PROCEDURE [dbo].[ReportDailyByKhachHangMatHang] 
 	-- Add the parameters for the stored procedure here
 	@date datetime
 AS
 BEGIN
-	select kh.TenKhachHang, mh.TenMatHang, ct.SoLuong
+	select kh.TenKhachHang, mh.TenMatHang, sum(ct.SoLuong) SoLuong
 	from dbo.tDonHang dh
 	join dbo.tChiTietDonHang ct on dh.Ma = ct.MaDonHang
 	join dbo.rKhachHang kh on dh.MaKhachHang = kh.Ma
 	join dbo.tMatHang mh on ct.MaMatHang = mh.Ma
 	where dh.Ngay = @date
-	group by kh.TenKhachHang, mh.TenMatHang, ct.SoLuong
+	group by kh.TenKhachHang, mh.TenMatHang
+	order by kh.TenKhachHang
 END
