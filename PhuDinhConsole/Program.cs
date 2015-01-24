9ec457d1b163e150639941ed24163aca6587ddf8
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2015-01-24 04:23:19 +0000

    - add PhuDinhConsole.csproj: handle schedule task like UpdateTonKho, ....

---
+++ PhuDinhConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PhuDinhConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.GlobalContext.Properties["name"] = DateTime.Now.ToString("yyyy_MM_dd-hh_mm_ss") + ".log";
+            PhuDinhData.TonKhoManager.UpdateTonKho();
+        }
+    }
+}
