17da4a2101997dabbc028495467b0731d6a16daa
phamquochuy <phamquochuy@655ddf45-47ed-0a24-b29b-296d970e0281>
2015-01-24 05:15:33 +0000

    add script to create rar file for release folder and put it in: C:\Users\Administrator\OneDrive\server\db\release

--- build.cmd
+++ build.cmd
@@ -1,1 +1,9 @@
 MsBuild.exe %~dp0\PhuDinh.sln /m /t:Build /p:Configuration=Release /p:TargetFramework=v4.0
+
+set d=%date:/=-%
+set t=%time::=_%
+set t=%t:,=_%
+set Rar="C:\Program Files\WinRAR\rar.exe"
+set OutputName="%d% %t%.rar"
+
+%Rar% a -hpnobita C:\Users\Administrator\OneDrive\server\db\release\%OutputName%.rar %~dp0\PhuDinh\bin\Release
