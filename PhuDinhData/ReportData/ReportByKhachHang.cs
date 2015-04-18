b1d917fcb450948cb78b83d9464c522b155990d3
kudo183 <kudo183@gmail.com>
2015-04-18 16:50:13 +0700

    - add KhachHang filter, option group by date or not. (need enhance code).

--- PhuDinhData/ReportData/ReportByKhachHang.cs
+++ PhuDinhData/ReportData/ReportByKhachHang.cs
@@ -16,17 +16,37 @@ namespace PhuDinhData.ReportData
             public string SoLuongText { get; set; }
         }
 
-        public static List<ReportByKhachHangData> FilterByDate(DateTime ngay)
+        public static List<ReportByKhachHangData> FilterByDate(DateTime ngay, bool isGroupByDate)
         {
-            return Filter(p => p.Ngay == ngay);
+            return Filter(p => p.Ngay == ngay, isGroupByDate);
         }
 
-        public static List<ReportByKhachHangData> FilterByDate(DateTime tuNgay, DateTime denNgay)
+        public static List<ReportByKhachHangData> FilterByDate(DateTime tuNgay, DateTime denNgay, bool isGroupByDate)
         {
-            return Filter(p => p.Ngay >= tuNgay && p.Ngay <= denNgay);
+            return Filter(p => p.Ngay >= tuNgay && p.Ngay <= denNgay, isGroupByDate);
         }
 
-        private static List<ReportByKhachHangData> Filter(System.Linq.Expressions.Expression<Func<tDonHang, bool>> filter)
+        public static List<ReportByKhachHangData> FilterByDate(DateTime ngay, string tenKhachHang, bool isGroupByDate)
+        {
+            return Filter(p => p.Ngay == ngay && p.rKhachHang.TenKhachHang.Contains(tenKhachHang), isGroupByDate);
+        }
+
+        public static List<ReportByKhachHangData> FilterByDate(DateTime tuNgay, DateTime denNgay, string tenKhachHang, bool isGroupByDate)
+        {
+            return Filter(p => p.Ngay >= tuNgay && p.Ngay <= denNgay && p.rKhachHang.TenKhachHang.Contains(tenKhachHang), isGroupByDate);
+        }
+
+        public static List<ReportByKhachHangData> FilterByDate(DateTime ngay, int maKhachHang, bool isGroupByDate)
+        {
+            return Filter(p => p.Ngay == ngay && p.rKhachHang.Ma == maKhachHang, isGroupByDate);
+        }
+
+        public static List<ReportByKhachHangData> FilterByDate(DateTime tuNgay, DateTime denNgay, int maKhachHang, bool isGroupByDate)
+        {
+            return Filter(p => p.Ngay >= tuNgay && p.Ngay <= denNgay && p.rKhachHang.Ma == maKhachHang, isGroupByDate);
+        }
+
+        private static List<ReportByKhachHangData> Filter(System.Linq.Expressions.Expression<Func<tDonHang, bool>> filter, bool isGroupByDate)
         {
             var context = ContextFactory.CreateContext();
 
@@ -45,14 +65,29 @@ namespace PhuDinhData.ReportData
                     TenKhachHang = donHang.Key
                 });
 
-                foreach (var gbNgay in donHang.GroupBy(p => p.Ngay))
+                if (isGroupByDate)
                 {
-                    result.Add(new ReportByKhachHangData()
+                    foreach (var gbNgay in donHang.GroupBy(p => p.Ngay))
                     {
-                        Ngay = gbNgay.Key.ToString("dd/MM/yyyy")
-                    });
-                    
-                    foreach (var matHang in GroupByMatHang(gbNgay))
+                        result.Add(new ReportByKhachHangData()
+                        {
+                            Ngay = gbNgay.Key.ToString("dd/MM/yyyy")
+                        });
+
+                        foreach (var matHang in GroupByMatHang(gbNgay))
+                        {
+                            result.Add(new ReportByKhachHangData()
+                            {
+                                TenMatHang = matHang.Value.Key,
+                                SoLuong = matHang.Value.Value,
+                                SoLuongText = matHang.Value.Value.ToString("N0")
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var matHang in GroupByMatHang(donHang))
                     {
                         result.Add(new ReportByKhachHangData()
                         {
--- PhuDinhReport/ReportByKhachHangView.xaml.cs
+++ PhuDinhReport/ReportByKhachHangView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CustomControl.DataGridColumnHeaderFilterModel;
+using PhuDinhData;
 using PhuDinhData.ReportData;
 using System;
 using System.Windows.Controls;
@@ -10,21 +13,42 @@ namespace PhuDinhReport
     /// </summary>
     public partial class ReportByKhachHangView : UserControl
     {
+        public HeaderComboBoxFilterModel Header_KhachHang { get; set; }
+
+        private int _mode = 1;
+        private bool _isGroupByDate = false;
+
         public ReportByKhachHangView()
         {
             InitializeComponent();
 
             reportDatePicker.NgaySelected += reportDatePicker_NgaySelected;
             reportDatePicker.TuNgayDenNgaySelected += reportDatePicker_TuNgayDenNgaySelected;
+
+            Header_KhachHang = new HeaderComboBoxFilterModel("Khach Hang");
+            var context = ContextFactory.CreateContext();
+            Header_KhachHang.ItemSource = context.rKhachHangs.OrderBy(p => p.TenKhachHang)
+                .ToDictionary(p => p.Ma, p => p.TenKhachHang);
+            Header_KhachHang.PropertyChanged += Header_KhachHang_PropertyChanged;
+            Header_KhachHang.SelectedValue = 162;
+
+            dg.Columns[0].Header = Header_KhachHang;
+        }
+
+        void Header_KhachHang_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Refresh();
         }
 
         void reportDatePicker_NgaySelected(object sender, EventArgs e)
         {
+            _mode = 1;
             ReportByDate();
         }
 
         void reportDatePicker_TuNgayDenNgaySelected(object sender, EventArgs e)
         {
+            _mode = 2;
             ReportFromDateToDate();
         }
 
@@ -32,7 +56,15 @@ namespace PhuDinhReport
         {
             var ngay = reportDatePicker.Ngay.Value.Date;
 
-            var result = ReportByKhachHang.FilterByDate(ngay);
+            List<ReportByKhachHang.ReportByKhachHangData> result;
+            if (Header_KhachHang.IsUsed)
+            {
+                result = ReportByKhachHang.FilterByDate(ngay, (int)Header_KhachHang.SelectedValue, _isGroupByDate);
+            }
+            else
+            {
+                result = ReportByKhachHang.FilterByDate(ngay, _isGroupByDate);
+            }
 
             reportDatePicker.NgayMsg = string.Format("Tong so cuon: {0}", result.Sum(p => p.SoLuong).ToString("N0"));
 
@@ -45,11 +77,44 @@ namespace PhuDinhReport
 
             var denNgay = reportDatePicker.DenNgay.Value.Date;
 
-            var result = ReportByKhachHang.FilterByDate(tuNgay, denNgay);
+            List<ReportByKhachHang.ReportByKhachHangData> result;
+            if (Header_KhachHang.IsUsed)
+            {
+                result = ReportByKhachHang.FilterByDate(tuNgay, denNgay, (int)Header_KhachHang.SelectedValue, _isGroupByDate);
+            }
+            else
+            {
+                result = ReportByKhachHang.FilterByDate(tuNgay, denNgay, _isGroupByDate);
+            }
 
             reportDatePicker.TuNgayDenNgayMsg = string.Format("Tong so cuon: {0}", result.Sum(p => p.SoLuong).ToString("N0"));
 
             dg.ItemsSource = result;
+        }
+
+        private void CheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _isGroupByDate = true;
+            Refresh();
+        }
+
+        private void CheckBox_Unchecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _isGroupByDate = false;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            switch (_mode)
+            {
+                case 1:
+                    ReportByDate();
+                    break;
+                case 2:
+                    ReportFromDateToDate();
+                    break;
+            }
         }
     }
 }
