7272814b45ef852541e6950333bb4aff46d7c90b
kudo183 <kudo183@gmail.com>
2015-06-16 15:41:02 +0700

    - set viewport, css, script, .... - add Google Sign In - add validation message.

--- PhuDinhWeb/Controllers/AccountController.cs
+++ PhuDinhWeb/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using System.Web.Security;
 using PhuDinhWeb.Models;
 
@@ -16,29 +20,19 @@ namespace PhuDinhWeb.Controllers
 
         //
         // POST: /Account/LogOn
-
         [HttpPost]
         public ActionResult LogOn(LogOnModel model, string returnUrl)
         {
             if (ModelState.IsValid)
             {
                 //if (Membership.ValidateUser(model.UserName, model.Password))
-                if (model.Password == "nobita")
+                if (IsValid(model))
                 {
-                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-                    {
-                        return Redirect(returnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "TonKho", new { id = "pd" });
-                    }
+                    return LoginSuccess(model, returnUrl);
                 }
                 else
                 {
-                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                    ModelState.AddModelError("", "Mat khau khong chinh xac.");
                 }
             }
 
@@ -54,6 +48,50 @@ namespace PhuDinhWeb.Controllers
             FormsAuthentication.SignOut();
 
             return RedirectToAction("LogOn", "Account");
+        }
+
+        private ActionResult LoginSuccess(LogOnModel model, string returnUrl)
+        {
+            FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+            if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return Redirect("/TonKho");
+            }
+        }
+
+        private bool IsValid(LogOnModel model)
+        {
+            if (model.Password == "nobita")
+                return true;
+
+            if (model.UserName == "Google")
+            {
+                try
+                {
+                    var client = new WebClient();
+                    var json = client.DownloadString(
+                        "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + model.Password);
+
+                    var serializer = new JavaScriptSerializer();
+
+                    var values = serializer.Deserialize<Dictionary<string, string>>(json);
+
+                    if (values["email"] == "kudo183@gmail.com")
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            return false;
         }
     }
 }
