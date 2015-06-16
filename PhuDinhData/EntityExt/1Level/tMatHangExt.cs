00152ef6537b4998bd6a74e1d8d7403a0234a5c1
kudo183 <kudo183@gmail.com>
2015-06-16 14:38:58 +0700

    - add validator.

--- PhuDinhData/EntityExt/1Level/tMatHangExt.cs
+++ PhuDinhData/EntityExt/1Level/tMatHangExt.cs
@@ -26,11 +26,13 @@ namespace PhuDinhData
         {
             base.Init();
 
+            _validators.Add("MaLoai", () => Common.MyValidators.Validate_NumberRange(this, "MaLoai", 1, double.MaxValue));
             _validators.Add("TenMatHang", () => Common.MyValidators.Validate_RequiredText(this, "TenMatHang"));
             _validators.Add("TenMatHangDayDu", () => Common.MyValidators.Validate_RequiredText(this, "TenMatHangDayDu"));
+            _validators.Add("TenMatHangIn", () => Common.MyValidators.Validate_RequiredText(this, "TenMatHangIn"));
             _validators.Add("SoKy", () => Common.MyValidators.Validate_NumberRange(this, "SoKy", 0, double.MaxValue));
             _validators.Add("SoMet", () => Common.MyValidators.Validate_NumberRange(this, "SoMet", 0, double.MaxValue));
-
+            
             Validate();
         }
     }
