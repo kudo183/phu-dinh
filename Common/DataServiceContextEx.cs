92fe5ef1321b462118b8709220e78602b4aeb728
kudo183 <kudo183@gmail.com>
2015-06-29 09:58:52 +0700

    - add WebClientEx and DataServiceContextEx.

---
+++ Common/DataServiceContextEx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.Services.Client;
+using System.Xml.Linq;
+
+namespace Common
+{
+    public class DataServiceContextEx : DataServiceContext
+    {
+        /// <summary>Constructor taking the service root in parameter.</summary>
+        /// <param name="serviceRoot"></param>
+        public DataServiceContextEx(Uri serviceRoot)
+            : base(serviceRoot)
+        {
+            WritingEntity += DataModelProxy_WritingEntity;
+        }
+
+        void DataModelProxy_WritingEntity(object sender, ReadingWritingEntityEventArgs e)
+        {
+            RemoveIgnoredProperties(ref e);
+        }
+
+        private static void RemoveIgnoredProperties(ref ReadingWritingEntityEventArgs e)
+        {
+            var attIgnoredProperties = e.Entity.GetType().GetCustomAttributes(typeof(System.Data.Services.IgnorePropertiesAttribute), true);
+            if (attIgnoredProperties.Length != 1)
+                return;
+
+            var ignoredProperties = (attIgnoredProperties[0] as System.Data.Services.IgnorePropertiesAttribute).PropertyNames;
+
+            foreach (XElement node in e.Data.Elements())
+            {
+                if (node != null && node.Name.LocalName == "content")
+                {
+                    foreach (XElement el in node.Elements())
+                    {
+                        if (el.Name.LocalName == "properties")
+                        {
+                            foreach (XElement prop in el.Elements())
+                            {
+                                if (ignoredProperties.Contains(prop.Name.LocalName) == true)
+                                    prop.Remove();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public DataServiceQuery<T> Set<T>() where T : Common.BindableObject
+        {
+            return CreateQuery<T>(typeof(T).Name + "s");
+        }
+
+        public void Add<T>(T entity) where T : Common.BindableObject
+        {
+            AddObject(typeof(T).Name + "s", entity);
+        }
+
+        public void Delete<T>(T entity) where T : Common.BindableObject
+        {
+            DeleteObject(entity);
+        }
+
+        public void Update<T>(T entity) where T : Common.BindableObject
+        {
+            UpdateObject(entity);
+        }
+    }
+}
---
+++ Common/WebClientEx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace Common
+{
+    public class WebClientEx : WebClient
+    {
+        private static volatile WebClientEx instance;
+        private static readonly object syncRoot = new Object();
+
+        private WebClientEx() { }
+
+        public CookieContainer CookieContainer
+        {
+            get { return _container; }
+        }
+
+        /// <summary>
+        /// Need call Initialize static method before use
+        /// </summary>
+        public static WebClientEx Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new WebClientEx();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private static readonly CookieContainer _container = new CookieContainer();
+
+        private static string _loginUrl;
+        private static string _logOffUrl;
+        private static string _getAntiForgeryTokenUrl;
+
+        public static void Initialize(string loginUrl, string logOffUrl, string getAntiForgeryTokenUrl)
+        {
+            _loginUrl = loginUrl;
+            _logOffUrl = logOffUrl;
+            _getAntiForgeryTokenUrl = getAntiForgeryTokenUrl;
+        }
+
+        public bool Login(string user, string pass)
+        {
+            var data = new NameValueCollection();
+            data["username"] = user;
+            data["password"] = pass;
+            data["__RequestVerificationToken"] = GetAntiForgeryToken();
+
+            UploadValues(_loginUrl, "POST", data);
+
+            return _container.Count == 2;
+        }
+
+        public void LogOff()
+        {
+            var data = new NameValueCollection();
+            data["__RequestVerificationToken"] = GetAntiForgeryToken();
+
+            UploadValues(_logOffUrl, "POST", data);
+        }
+
+        private string GetAntiForgeryToken()
+        {
+            var htmlToken = DownloadString(_getAntiForgeryTokenUrl);
+
+            var token = htmlToken.Replace("<input name=\"__RequestVerificationToken\" type=\"hidden\" value=\"", "").Replace("\" />", "");
+
+            return token;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest r = base.GetWebRequest(address);
+            var request = r as HttpWebRequest;
+            if (request != null)
+            {
+                request.CookieContainer = _container;
+            }
+            return r;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request, IAsyncResult result)
+        {
+            WebResponse response = base.GetWebResponse(request, result);
+            ReadCookies(response);
+            return response;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            WebResponse response = base.GetWebResponse(request);
+            ReadCookies(response);
+            return response;
+        }
+
+        private void ReadCookies(WebResponse r)
+        {
+            var response = r as HttpWebResponse;
+            if (response != null)
+            {
+                CookieCollection cookies = response.Cookies;
+                _container.Add(cookies);
+            }
+        }
+    }
+}
