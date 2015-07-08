3db72e7cb9463aa6eb74bb1601652213430bf61e
kudo183 <kudo183@gmail.com>
2015-07-08 16:09:20 +0700

    - merge 2 trigger into new trigger to ensure trigger work correctly because 2 trigger order in not predicated

--- Database/dbo/Tables/tChiTietChuyenHangDonHang.sql
+++ Database/dbo/Tables/tChiTietChuyenHangDonHang.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[tChiTietChuyenHangDonHang] (
+CREATE TABLE [dbo].[tChiTietChuyenHangDonHang] (
     [Ma]                  INT IDENTITY (1, 1) NOT NULL,
     [MaChuyenHangDonHang] INT NOT NULL,
     [MaChiTietDonHang]    INT NOT NULL,
@@ -8,6 +8,8 @@
     CONSTRAINT [FK_tChiTietChuyenHangDonHang_tChiTietDonHang] FOREIGN KEY ([MaChiTietDonHang]) REFERENCES [dbo].[tChiTietDonHang] ([Ma]),
     CONSTRAINT [FK_tChiTietChuyenHangDonHang_tChuyenHangDonHang] FOREIGN KEY ([MaChuyenHangDonHang]) REFERENCES [dbo].[tChuyenHangDonHang] ([Ma])
 );
+
+
 
 
 
@@ -51,33 +53,25 @@ CREATE TRIGGER [dbo].[tChiTietChuyenHangDonHang_trUpdateXong]
 		and SoLuong = (select sum(SoLuong) from [dbo].[tChiTietChuyenHangDonHang] where MaChiTietDonHang = t.Ma)					
 	END
 GO
+
+GO
 -- Batch submitted through debugger: SQLQuery2.sql|7|0|C:\Users\ADMINI~1\AppData\Local\Temp\~vsF671.sql
-CREATE TRIGGER [dbo].[tChiTietChuyenHangDonHang_trUpdateChuyenHangDonHang_TongSoLuongThucTe]
+create TRIGGER [dbo].[tChiTietChuyenHangDonHang_trUpdateTongSoLuongThucTe]
 	ON [dbo].[tChiTietChuyenHangDonHang]
 	after DELETE, INSERT, UPDATE
 	AS
 	BEGIN
 		SET NOCOUNT ON
-
+		
 		update tChuyenHangDonHang
 		set TongSoLuongThucTe = (select ISNULL(sum(SoLuong), 0) from tChiTietChuyenHangDonHang where MaChuyenHangDonHang = tChuyenHangDonHang.Ma)
 		from (select i.MaChuyenHangDonHang from inserted i union select d.MaChuyenHangDonHang from deleted d) as t
 		where tChuyenHangDonHang.Ma	 = t.MaChuyenHangDonHang
-	END
-GO
--- Batch submitted through debugger: SQLQuery2.sql|7|0|C:\Users\ADMINI~1\AppData\Local\Temp\~vsF671.sql
-create TRIGGER [dbo].[tChiTietChuyenHangDonHang_trUpdateChuyenHang_TongSoLuongThucTe]
-	ON [dbo].[tChiTietChuyenHangDonHang]
-	after DELETE, INSERT, UPDATE
-	AS
-	BEGIN
-		SET NOCOUNT ON
 
 		update tChuyenHang
 		set TongSoLuongThucTe = 
-			(select ISNULL(sum(SoLuong), 0)
-			from tChiTietChuyenHangDonHang ctchdh
-			join tChuyenHangDonHang chdh on chdh.Ma = ctchdh.MaChuyenHangDonHang
+			(select ISNULL(sum(TongSoLuongThucTe), 0)
+			from tChuyenHangDonHang chdh
 			where chdh.MaChuyenHang = tChuyenHang.Ma)
 		from (select i.MaChuyenHangDonHang from inserted i union select d.MaChuyenHangDonHang from deleted d) as t
 		join tChuyenHangDonHang chdh on t.MaChuyenHangDonHang = chdh.Ma
--- Database/dbo/Tables/tChuyenHangDonHang.sql
+++ Database/dbo/Tables/tChuyenHangDonHang.sql
@@ -12,34 +12,12 @@
 
 
 
-GO
--- Batch submitted through debugger: SQLQuery2.sql|7|0|C:\Users\ADMINI~1\AppData\Local\Temp\~vsF671.sql
-CREATE TRIGGER [dbo].[tChuyenHangDonHang_trUpdateChuyenHangDonHang_TongSoLuongTheoDonHang]
-	ON [dbo].[tChuyenHangDonHang]
-	after INSERT, UPDATE
-	AS
-	BEGIN
-		SET NOCOUNT ON
 
-		update tChuyenHangDonHang
-		set TongSoLuongTheoDonHang = (select TongSoLuong from tDonHang where Ma = tChuyenHangDonHang.MaDonHang)
-		from inserted i
-		where tChuyenHangDonHang.Ma = i.Ma
-	END
-GO
--- Batch submitted through debugger: SQLQuery2.sql|7|0|C:\Users\ADMINI~1\AppData\Local\Temp\~vsF671.sql
-CREATE TRIGGER [dbo].[tChuyenHangDonHang_trUpdateChuyenHang_TongSoLuongTheoDonHang]
-	ON [dbo].[tChuyenHangDonHang]
-	after DELETE, INSERT, UPDATE
-	AS
-	BEGIN
-		SET NOCOUNT ON
 
-		update tChuyenHang
-		set TongSoLuongTheoDonHang = (select ISNULL(sum(TongSoLuongTheoDonHang), 0) from tChuyenHangDonHang where MaChuyenHang = tChuyenHang.Ma)
-		from (select i.MaChuyenHang from inserted i union select d.MaChuyenHang from deleted d) as t
-		where tChuyenHang.Ma = t.MaChuyenHang
-	END
+GO
+
+GO
+
 GO
 -- Batch submitted through debugger: SQLQuery2.sql|7|0|C:\Users\ADMINI~1\AppData\Local\Temp\~vsF671.sql
 CREATE TRIGGER [dbo].[tChuyenHangDonHang_trUpdateChuyenHang_TongDonHang]
@@ -53,4 +31,53 @@ CREATE TRIGGER [dbo].[tChuyenHangDonHang_trUpdateChuyenHang_TongDonHang]
 		set TongDonHang = (select count(*) from tChuyenHangDonHang where MaChuyenHang = tChuyenHang.Ma)
 		from (select i.MaChuyenHang from inserted i union select d.MaChuyenHang from deleted d) as t
 		where tChuyenHang.Ma = t.MaChuyenHang
+	END
+GO
+-- Batch submitted through debugger: SQLQuery2.sql|7|0|C:\Users\ADMINI~1\AppData\Local\Temp\~vsF671.sql
+CREATE TRIGGER [dbo].[tChuyenHangDonHang_trUpdateTongSoLuongTheoDonHang]
+	ON [dbo].[tChuyenHangDonHang]
+	after DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+		Declare @Evt Int = 0;
+
+		SET NOCOUNT ON
+
+		IF(EXISTS(SELECT * FROM inserted))
+			Set @Evt = @Evt + 1;
+		IF(EXISTS(SELECT * FROM deleted))
+			Set @Evt = @Evt + 2;
+		
+		IF(@Evt = 1) -- inserted
+		begin
+			update tChuyenHangDonHang
+			set TongSoLuongTheoDonHang = (select TongSoLuong from tDonHang where Ma = tChuyenHangDonHang.MaDonHang)
+			from inserted i
+			where tChuyenHangDonHang.Ma = i.Ma
+			update tChuyenHang
+			set TongSoLuongTheoDonHang = (select ISNULL(sum(TongSoLuongTheoDonHang), 0) from tChuyenHangDonHang where MaChuyenHang = tChuyenHang.Ma)
+			from (select i.MaChuyenHang from inserted i union select d.MaChuyenHang from deleted d) as t
+			where tChuyenHang.Ma = t.MaChuyenHang
+		end
+		else IF(@Evt = 2) -- deleted
+		begin
+			update tChuyenHang
+			set TongSoLuongTheoDonHang = (select ISNULL(sum(TongSoLuongTheoDonHang), 0) from tChuyenHangDonHang where MaChuyenHang = tChuyenHang.Ma)
+			from (select i.MaChuyenHang from inserted i union select d.MaChuyenHang from deleted d) as t
+			where tChuyenHang.Ma = t.MaChuyenHang
+		end
+		else IF(@Evt = 3) -- updated
+		begin
+			if(update(MaDonHang))
+			begin
+				update tChuyenHangDonHang
+				set TongSoLuongTheoDonHang = (select TongSoLuong from tDonHang where Ma = tChuyenHangDonHang.MaDonHang)
+				from inserted i
+				where tChuyenHangDonHang.Ma = i.Ma
+				update tChuyenHang
+				set TongSoLuongTheoDonHang = (select ISNULL(sum(TongSoLuongTheoDonHang), 0) from tChuyenHangDonHang where MaChuyenHang = tChuyenHang.Ma)
+				from (select i.MaChuyenHang from inserted i union select d.MaChuyenHang from deleted d) as t
+				where tChuyenHang.Ma = t.MaChuyenHang
+			end
+		end
 	END
