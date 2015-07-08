26cb039e8e31f1298a22b5255738ed5f79b5febb
kudo183 <kudo183@gmail.com>
2015-07-08 16:49:25 +0700

    - add DataGridReadOnlyCheckBoxColumn.xaml for all read only check box column.

---
+++ CustomControl/DataGridColumn/CheckBoxColumn/DataGridCheckBoxColumnExt.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridCheckBoxColumnExt.xaml
+    /// </summary>
+    public partial class DataGridCheckBoxColumnExt : DataGridCheckBoxColumn
+    {
+        public DataGridCheckBoxColumnExt()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CustomControl/DataGridColumn/CheckBoxColumn/DataGridReadOnlyCheckBoxColumn.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DataGridReadOnlyCheckBoxColumn.xaml
+    /// </summary>
+    public partial class DataGridReadOnlyCheckBoxColumn : DataGridCheckBoxColumn
+    {
+        public DataGridReadOnlyCheckBoxColumn()
+        {
+            InitializeComponent();
+        }
+    }
+}
