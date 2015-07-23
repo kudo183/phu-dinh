857c75f2a72c77dee3e0b14823257714f3f0279f
kudo183 <kudo183@gmail.com>
2015-07-23 08:33:31 +0700

    *fixbug ServiceLocator not Initialized.

--- PhuDinhConsole/Program.cs
+++ PhuDinhConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using PhuDinhCommon;
+using PhuDinhEFClientContext;
 
 namespace PhuDinhConsole
 {
@@ -6,6 +9,12 @@ namespace PhuDinhConsole
     {
         static void Main(string[] args)
         {
+            Common.ServiceLocator.Instance.Initialize(new Dictionary<Type, Type>()
+            {
+                { typeof(IClientContextManager<>), typeof(EFContextManager<>) },
+                { typeof(IClientContext), typeof(EFContext) }
+            });
+
             log4net.GlobalContext.Properties["name"] = DateTime.Now.ToString("yyyy_MM_dd-hh_mm_ss") + ".log";
             PhuDinhData.TonKhoManager.UpdateTonKho();
         }
