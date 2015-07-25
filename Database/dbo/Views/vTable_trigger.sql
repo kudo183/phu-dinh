75e9e9d63d7cdc849721c360f760d037ae8e1a50
kudo183 <kudo183@gmail.com>
2015-07-25 11:40:35 +0700

    - add vTable_trigger.sql to list all trigger.

---
+++ Database/dbo/Views/vTable_trigger.sql
@@ -0,0 +1,11 @@
+﻿
+
+CREATE VIEW [dbo].[vTable_trigger]
+AS
+SELECT  
+       TAB.name as Table_Name 
+     , TRIG.name as Trigger_Name
+     , TRIG.is_disabled  --or objectproperty(object_id('TriggerName'), 'ExecIsTriggerDisabled')
+FROM [sys].[triggers] as TRIG 
+inner join sys.tables as TAB 
+on TRIG.parent_id = TAB.object_id
