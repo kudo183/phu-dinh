0e44266685189bf1b6e69c5efc06ef0497000ce3
kudo183 <kudo183@gmail.com>
2016-01-02 15:42:52 +0700

    - add button Copy Ton Kho Hang Nhap in TonKhoView.

--- PhuDinhCommonControl/View/2Level/tTonKhoView.xaml.cs
+++ PhuDinhCommonControl/View/2Level/tTonKhoView.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using PhuDinhData.ViewModel;
@@ -61,6 +65,48 @@ namespace PhuDinhCommonControl
             if (button.Content.ToString() == "Cap Nhat Ton Kho")
             {
                 ChildWindowUtils.ShowChildWindow("Cap Nhat Ton Kho", new CapNhatTonKhoView());
+                return;
+            }
+
+            if (button.Content.ToString() == "Copy Ton Kho Hang Nhap")
+            {
+                var maLoaiHang_HangNhap = new[] { 11, 1026, 1025, 1024, 17, 21, 1023, 8, 15 };
+                var filterDate = (_viewModel as TonKhoViewModel).Header_Ngay.Date;
+                var tTonKhos = PhuDinhData.ClientContext.Instance
+                   .GetDataWithRelated<PhuDinhDataEntity.tTonKho>(
+                       p => p.Ngay == filterDate && maLoaiHang_HangNhap.Contains(p.tMatHang.MaLoai),
+                       new List<string> { "tMatHang" })
+                   .OrderBy(p => p.tMatHang.TenMatHangDayDu)
+                   .ToList();
+
+                var builder = new StringBuilder();
+
+                builder.AppendLine(string.Format("{0:dd/MM/yyyy}\t\t{1:N0} cuộn"
+                    , filterDate, tTonKhos.Sum(p => p.SoLuong)));
+
+                int count = tTonKhos.Count;
+                var half = count / 2;
+                for (int i = 0; i < half; i++)
+                {
+                    builder.Append(tTonKhos[i].tMatHang.TenMatHangDayDu);
+                    builder.Append("\t");
+                    builder.Append(tTonKhos[i].SoLuong);
+                    builder.Append("\t\t");
+                    builder.Append(tTonKhos[half + i].tMatHang.TenMatHangDayDu);
+                    builder.Append("\t");
+                    builder.Append(tTonKhos[half + i].SoLuong);
+                    builder.AppendLine("");
+                }
+
+                if ((count & 1) == 1)
+                {
+                    builder.Append(tTonKhos[count - 1].tMatHang.TenMatHangDayDu);
+                    builder.Append("\t");
+                    builder.Append(tTonKhos[count - 1].SoLuong);
+                    builder.AppendLine("");
+                }
+
+                Clipboard.SetText(builder.ToString());
                 return;
             }
 
